cd7a5eb99f3acd45b1ac28fbe8cd935534f034ec
Dhruv Suthar <intelligentjimmy@gmail.com>
2021-03-08 10:16:47 +0530

    fix: edit Garage

--- Models/Garages/UpdateGarageModel.cs
+++ Models/Garages/UpdateGarageModel.cs
@@ -4,6 +4,7 @@
     {
         public string Name { get; set; }
         public string Address { get; set; }
+        public string City { get; set; }
         public string State { get; set; }
         public string Phone { get; set; }
         public bool hasCleaningService { get; set; }
